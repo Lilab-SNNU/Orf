cd08f2997f08cddc37e539374e15d91b3c34ee49
RU_i <76428645+13720550513@users.noreply.github.com>
2021-12-05 04:42:14 +0800

    Delete one_step_script.sh

--- one_step_script.sh
+++
@@ -1,9 +0,0 @@
-#!user/bin/bash
-
-
-python make_index_genomes.py -y config.yaml
-python filter_coding_reads.py -y config.yaml
-python filter_coding_circ.py -y config.yaml
-
-python filter_coding_orf.py -y config.yaml
-python visual_circ_orf.py -y config.yaml
